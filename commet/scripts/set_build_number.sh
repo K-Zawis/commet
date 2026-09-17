0bbb79ab750abd3e36d119e677e1479c5a0a2af6
K-Zawis <zawistowska.kasia@outlook.com>
2026-09-17 22:28:52 +0100

    fix(*): minor fix assigning bash type for the script and seperating export and var to catch any issues

--- commet/scripts/set_build_number.sh
+++ commet/scripts/set_build_number.sh
@@ -1,2 +1,6 @@
-export BUILD_NUMBER=`git rev-list --count main`
+#!/usr/bin/env bash
+
+BUILD_NUMBER=$(git rev-list --count prod)
+export BUILD_NUMBER
+
 perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.$ENV{BUILD_NUMBER}/e' pubspec.yaml
