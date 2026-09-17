914e0e6cf20bca025cabd864cef5badb173e6099
Kasia Zawistowska <95303415+K-Zawis@users.noreply.github.com>
2026-09-17 22:52:25 +0100

    fix(prepare-web.sh): Cargo.toml was looking for a parent project that does not exist

--- commet/scripts/prepare-web.sh
+++ commet/scripts/prepare-web.sh
@@ -4,6 +4,11 @@
 git clone https://github.com/famedly/dart-vodozemac.git .vodozemac
 cd .vodozemac
 git checkout 0.5.0
+
+# Detach from parent workspace
+echo "" >> rust/Cargo.toml
+echo "[workspace]" >> rust/Cargo.toml
+
 cargo install flutter_rust_bridge_codegen
 flutter_rust_bridge_codegen build-web --dart-root dart --rust-root $(readlink -f rust) --release
 cd ..
